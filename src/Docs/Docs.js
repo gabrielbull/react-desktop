7b7d0c9979f152447f5e3b9807cfdba3a1084be2
Gabriel Bull <me@gabrielbull.com>
2015-10-17 01:36:25 -0400

    Added docs

--- src/Docs/Docs.js
+++ src/Docs/Docs.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { history } from '../Router';
-import ReadMe from '../../raw-docs/README.html';
+import ReadMe from '../../raw-docs/index.html';
 
 export default class extends Component {
   componentDidMount() {
+    this.bindLinkEvents();
+  }
+
+  componentDidUpdate() {
+    this.bindLinkEvents();
+  }
+
+  bindLinkEvents() {
     const links = this.refs.element.getElementsByTagName('a');
     for(var prop in links) {
-        if(links.hasOwnProperty(prop)) {
-          links[prop].addEventListener('click', this.handleLink);
-        }
+      if(links.hasOwnProperty(prop)) {
+        links[prop].addEventListener('click', this.handleLink);
+      }
     }
   }
 
   handleLink(event) {
-    event.preventDefault();
-    history.pushState(null, event.currentTarget.getAttribute('href'));
+    const link = event.currentTarget.getAttribute('href');
+    if (!link.match(/^http/)) {
+      event.preventDefault();
+      console.log('push stste?');
+      history.pushState(null, event.currentTarget.getAttribute('href'));
+    }
   }
 
   render() {
+    let content;
+    if (this.props.params.splat) {
+      if (this.props.location.pathname.match(/\/$/)) {
+        content = require(`../../raw-docs/${this.props.params.splat}/index.html`);
+      } else {
+        content = require(`../../raw-docs/${this.props.params.splat}.html`);
+      }
+    } else {
+      content = require('../../raw-docs/index.html');
+    }
+
     return (
       <div ref="element">
-        <div dangerouslySetInnerHTML={{__html: ReadMe}} />
+        <div dangerouslySetInnerHTML={{__html: content}} />
       </div>
     );
   }
