06b071f8ff7e6d6d6b19afb52ac8a5e81118ef14
Gabriel Bull <me@gabrielbull.com>
2015-10-16 23:52:56 -0400

    Added uglify to js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 var createPages = function () {
   gulp.src('./index.html')
@@ -8,8 +9,16 @@ var createPages = function () {
     .pipe(gulp.dest('./docs/'))
 };
 
+var compress = function() {
+  return gulp.src('build/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+};
+
 gulp.task('create-pages', createPages);
+gulp.task('compress', compress);
 
 gulp.task('bundle',  function () {
   createPages();
+  compress();
 });
