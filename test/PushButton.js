1496ea0669addb88956adb9641578af8bf02779b
Salakar <mike.diarmid@gmail.com>
2015-10-17 15:26:03 +0100

    Fixed button test

--- test/PushButton.js
+++ test/PushButton.js
@@ -1,21 +1,21 @@
 import chai, { expect } from 'chai';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import PushButton from '../src/PushButton';
+import Button from '../src/Button';
 
 describe('PushButton', () => {
   it('create button component with text', () => {
-    const string = ReactDOMServer.renderToString(<PushButton>Hello</PushButton>);
+    const string = ReactDOMServer.renderToString(<Button>Hello</Button>);
     expect(string).to.match(/Hello/);
   });
 
   it('create button component with one node', () => {
-    const string = ReactDOMServer.renderToString(<PushButton><span>Hello</span></PushButton>);
+    const string = ReactDOMServer.renderToString(<Button><span>Hello</span></Button>);
     expect(string).to.match(/<span.*>Hello/);
   });
 
   it('create button component with multiple nodes', () => {
-    const string = ReactDOMServer.renderToString(<PushButton><span>Hello</span><span>World</span></PushButton>);
+    const string = ReactDOMServer.renderToString(<Button><span>Hello</span><span>World</span></Button>);
     expect(string).to.match(/<span.*>Hello/);
     expect(string).to.match(/<span.*>World/);
   });
