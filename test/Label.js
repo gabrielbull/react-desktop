a2b49bd0112431bd3804ed2a43f999cd81ad7929
C.T. Lin <chentsulin@gmail.com>
2015-10-19 08:39:48 +0800

    tweak import in test

--- test/Label.js
+++ test/Label.js
@@ -1,21 +1,21 @@
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import Label from '../src/Label';
 
 describe('Label', () => {
   it('create label component with text', () => {
-    const string = ReactDOMServer.renderToString(<Label>Hello</Label>);
+    const string = renderToString(<Label>Hello</Label>);
     expect(string).to.match(/Hello/);
   });
 
   it('create label component with one node', () => {
-    const string = ReactDOMServer.renderToString(<Label><span>Hello</span></Label>);
+    const string = renderToString(<Label><span>Hello</span></Label>);
     expect(string).to.match(/<span.*>Hello/);
   });
 
   it('create label component with multiple nodes', () => {
-    const string = ReactDOMServer.renderToString(<Label><span>Hello</span><span>World</span></Label>);
+    const string = renderToString(<Label><span>Hello</span><span>World</span></Label>);
     expect(string).to.match(/<span.*>Hello/);
     expect(string).to.match(/<span.*>World/);
   });
