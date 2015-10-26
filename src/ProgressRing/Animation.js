350ecd9471788ae3cd9c0a61b047404e1b16065b
Gabriel Bull <me@gabrielbull.com>
2015-10-26 19:58:40 -0400

    Simplified progress ring

--- src/ProgressRing/Animation.js
+++ src/ProgressRing/Animation.js
@@ -9,75 +9,48 @@ if (window) {
 }
 
 const totalIterations = 95;
-const cireclesInterval = 220;
+const circlesInterval = 14;
 const restartInterval = 250;
 const stopRotationAt = totalIterations * 2 / 1.02;
 const easing = BezierEasing(0, 0.47, 0.9, .25);
-let bounding;
+const bounding = 68;
 
-function rotateCircle(circle) {
-  const value = easing.get(1 / totalIterations * this.iteration) * 2 * Math.PI * -1;
-
-  circle.setAttributeNS(null, 'fill-opacity', 1);
-  circle.setAttributeNS(null, 'cx', 75 + (bounding * Math.sin(value)));
-  circle.setAttributeNS(null, 'cy', 75 + (bounding * Math.cos(value)));
-
-  this.currentIteration++;
-  if (this.iteration < totalIterations) {
-    this.iteration++;
-  } else {
-    this.iteration = 0;
-  }
-
-  if (this.readyCallback) {
-    this.readyCallback();
-    this.readyCallback = null;
-  }
-
-  if (this.currentIteration >= stopRotationAt) {
-    if (this.endCallback) {
-      this.endCallback();
-      this.endCallback = null;
+function rotateCircle(circles) {
+  let lastFrame = false;
+  for (var i = 0, len = circles.length; i < len; ++i) {
+    if (this.iteration >= circlesInterval * i) {
+      let iteration = this.iteration - circlesInterval * i;
+      const revolution = Math.floor(iteration / totalIterations);
+      iteration = iteration - (revolution * totalIterations);
+      if (iteration < 0) {
+        iteration = totalIterations - iteration;
+      } else if (iteration > totalIterations) {
+        iteration = iteration - totalIterations;
+      }
+      if (iteration + (revolution * totalIterations) > stopRotationAt) {
+        circles[i].setAttributeNS('', 'fill-opacity', '0');
+        if (i === circles.length-1) {
+          lastFrame = true;
+        }
+      } else {
+        const value = easing.get(1 / totalIterations * iteration) * 2 * Math.PI * -1;
+        circles[i].setAttributeNS('', 'fill-opacity', '1');
+        circles[i].setAttributeNS('', 'cx', 75 + (bounding * Math.sin(value)) + '');
+        circles[i].setAttributeNS('', 'cy', 75 + (bounding * Math.cos(value)) + '');
+      }
     }
-
-    circle.setAttributeNS(null, 'fill-opacity', 0);
-    return;
   }
 
-  requestAnimationFrame(rotateCircle.bind(this, circle));
+  this.iteration++;
+  if (!lastFrame) {
+    requestAnimationFrame(rotateCircle.bind(this, circles));
+  } else {
+    setTimeout(startAnimation.bind(null, ...circles), restartInterval);
+  }
 }
 
-function animateCircle(circle, last) {
-  return new Promise(function(resolve) {
-    rotateCircle.apply({
-      iteration: 0,
-      currentIteration: 0,
-      readyCallback: !last ? () => setTimeout(function () {
-        resolve();
-      }, cireclesInterval) : null,
-      endCallback: last ?  () => setTimeout(function () {
-        resolve();
-      }, restartInterval) : null
-    }, [circle]);
-  });
-}
-
-let currentTry = 0;
 export function startAnimation(...elements) {
   if (requestAnimationFrame) {
-    if (elements[0].getBBox().width === 0 && currentTry <= 100) {
-      currentTry++;
-      setTimeout(() => startAnimation(...elements), 100);
-      return;
-    }
-    bounding = 75 - elements[0].getBBox().width / 2;
-
-    animateCircle(elements[0])
-      .then(animateCircle.bind(null, elements[1]))
-      .then(animateCircle.bind(null, elements[2]))
-      .then(animateCircle.bind(null, elements[3]))
-      .then(animateCircle.bind(null, elements[4]))
-      .then(animateCircle.bind(null, elements[5], true))
-      .then(startAnimation.bind(null, ...elements));
+    rotateCircle.apply({iteration: 0, currentIteration: 0}, [elements]);
   }
 }
