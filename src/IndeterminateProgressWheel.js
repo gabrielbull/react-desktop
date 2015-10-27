e4f6e8095b43c2a3e1dba5cafe7431c17d1f322f
Gabriel Bull <me@gabrielbull.com>
2015-10-26 20:20:01 -0400

    Fixed bug with osx progress wheel path

--- src/IndeterminateProgressWheel.js
+++ src/IndeterminateProgressWheel.js
@@ -2,6 +2,6 @@ import Desktop from './Desktop';
 import ProgressRingWindows
   from './ProgressRing/ProgressRing.windows';
 import IndeterminateCircularProgressIndicatorOSX from
-  'IndeterminateCircularProgressIndicator/IndeterminateCircularProgressIndicator.osx';
+  './IndeterminateCircularProgressIndicator/IndeterminateCircularProgressIndicator.osx';
 
 export default Desktop.os === 'win' ? ProgressRingWindows : IndeterminateCircularProgressIndicatorOSX;
