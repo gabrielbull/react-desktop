3e5f60ae84e1b12bfdf152b16c482be665691987
Gabriel Bull <me@gabrielbull.com>
2016-01-18 11:24:14 -0500

    Renamed all tests

---
+++ test/button.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Button from '../src/button/button.windows/button';
+
+describe('Button', () => {
+  it('create button component with text', () => {
+    const string = renderToString(<Button>Hello</Button>);
+    expect(string).to.match(/Hello/);
+  });
+
+  it('create button component with one node', () => {
+    const string = renderToString(<Button><span>Hello</span></Button>);
+    expect(string).to.match(/<span.*>Hello/);
+  });
+
+  it('create button component with multiple nodes', () => {
+    const string = renderToString(<Button><span>Hello</span><span>World</span></Button>);
+    expect(string).to.match(/<span.*>Hello/);
+    expect(string).to.match(/<span.*>World/);
+  });
+});
