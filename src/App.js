ac4f4a52f33c5fac3a9f570643979df6c39e97d7
Gabriel Bull <me@gabrielbull.com>
2016-05-04 14:58:47 -0400

    Removed files for renaming

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Shared/Header/Header';
-import Routes from './Router';
-import styles from './Shared/Document/Document.scss';
-
-ReactDOM.render(<Header/>, document.getElementById('header'));
-ReactDOM.render(Routes, document.getElementById('main'));
--- src/Examples/Windows.js
+++
@@ -1,1 +0,0 @@
-export { default as SplitView } from './Windows/SplitView';
