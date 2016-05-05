ee4b6fa61a97ac6921bad9802e3d1a4ae92d0255
Gabriel Bull <me@gabrielbull.com>
2016-05-04 23:45:48 -0400

    Added redirection

--- src/app.js
+++ src/app.js
@@ -4,9 +4,5 @@ import Header from './shared/header/header';
 import Routes from './router';
 import './shared/document/document.scss';
 
-if(/http:\/\/(www\.)?github\.io/.test(window.location.href)) {
-  window.location = "http://reactdesktop.js.org";
-}
-
 ReactDOM.render(<Header/>, document.getElementById('header'));
 ReactDOM.render(Routes, document.getElementById('main'));
