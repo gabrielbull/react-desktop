82b88a0cef906651bb309e7bf31e03f812e8b43e
Gabriel Bull <me@gabrielbull.com>
2016-05-06 14:09:09 -0400

    Finished new osx demo

--- src/demo/window.osx.js
+++ src/demo/window.osx.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import {
   Window,
   TitleBar,
-  //PushButton,
   TextInput,
   Toolbar,
   SegmentedControl,
   ProgressCircle,
-  //Form,
   Checkbox,
   Radio,
   View,
   Button,
+  Text,
   Label
 } from 'react-desktop/osx';
 
@@ -33,10 +31,9 @@ export default class extends Component {
     }, 2000);
   };
 
-  cancel() {
-    this.refs.error.setState({display: false});
-    this.refs.loader.setState({visible: false});
-  }
+  cancel = () => {
+    this.setState({ showLoader: false, showError: false });
+  };
 
   render() {
     return (
@@ -47,7 +44,7 @@ export default class extends Component {
 
         <SegmentedControl box>
           <SegmentedControl.Item
-            title="Segmented"
+            title="Forms"
             selected={this.state.selectedTab === 'segmented'}
             onSelect={() => this.setState({ selectedTab: 'segmented' })}
           >
@@ -58,33 +55,39 @@ export default class extends Component {
 
               <TextInput
                 width="300px"
-                label="My Input"
+                label="This is an input"
                 placeholder="My Input"
                 margin="0 0 10px 0"
                 onChange={this.change}
               />
 
-              <View width="300px">
+              <View width="300px" margin="0 0 10px 0">
                 <Radio
-                  label="Check me!"
+                  label="This is a radio button"
                   name="radio1"
-                  onChange={(e) => console.log(e.target.value)}
-                  defaultValue="I got checked!"
                   defaultChecked
                 />
                 <Radio
-                  label="Check me!"
+                  label="This is another radio button"
                   name="radio1"
-                  onChange={(e) => console.log(e.target.value)}
-                  defaultValue="I got checked!"
+                />
+              </View>
+
+              <View width="300px">
+                <Checkbox
+                  label="This is a checkbox"
+                  defaultChecked
+                />
+                <Checkbox
+                  label="This is another checkbox"
                 />
               </View>
 
               <View direction="row" margin="14px 0 0 0">
-                <Button style={{ marginRight: '10px' }}>
+                <Button onClick={this.cancel} style={{ marginRight: '10px' }}>
                   Cancel
                 </Button>
-                <Button color="blue" onClick={this.submit}>
+                <Button type="submit" color="blue" onClick={this.submit}>
                   Submit
                 </Button>
                 <ProgressCircle
@@ -94,50 +97,15 @@ export default class extends Component {
               </View>
             </View>
           </SegmentedControl.Item>
+          <SegmentedControl.Item
+            title="Example"
+            selected={this.state.selectedTab === 'example'}
+            onSelect={() => this.setState({ selectedTab: 'example' })}
+          >
+            <Text>Hello World</Text>
+          </SegmentedControl.Item>
         </SegmentedControl>
       </Window>
     );
   }
 }
-/*
- <Box className="box">
- <SegmentedControl>
- <SegmentedControl.Item
- title="Segmented"
- selected={this.state.selectedTab === 'segmented'}
- onPress={() => { this.setState({ selectedTab: 'segmented' }) }}
- className="form"
- >
- <Form onSubmit={this.submit.bind(this)}>
- <Label ref="error" color="red" align="center" display={false}>
- There was an error submitting this form.
- </Label>
-
- <Form.Row>
- <Label>Label:</Label>
- <TextField defaultValue="" placeholder="TextField" style={{width: '200px'}}/>
- </Form.Row>
-
- <Form.Row>
- <Label>Longer label:</Label>
- <TextField defaultValue="" placeholder="TextField" style={{width: '200px'}}/>
- </Form.Row>
-
- <Form.Row>
- <PushButton onPress={this.cancel.bind(this)}>PushButton</PushButton>
- <PushButton onPress="submit" color="blue">PushButton Blue</PushButton>
-
- <IndeterminateCircularProgressIndicator ref="loader" absolute visible={false}/>
- </Form.Row>
- </Form>
- </SegmentedControl.Item>
-
- <SegmentedControl.Item
- title="Control"
- selected={this.state.selectedTab === 'control'}
- onPress={() => { this.setState({ selectedTab: 'control' }) }}
- >
- <Label>Hello World</Label>
- </SegmentedControl.Item>
- </SegmentedControl>
- </Box>*/
