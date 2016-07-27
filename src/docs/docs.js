fc81a658d3694ff25794b0d8ace53286157f66a6
Gabriel Bull <me@gabrielbull.com>
2016-07-27 15:02:30 -0400

    Update docs and dependencies

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -72,6 +72,7 @@ export default class extends Component {
     let ExampleContent;
     try {
       let name = this.props.params.splat.replace(/-./g, match => match.substr(1).toUpperCase());
+      name = name.replace(/\//, '/components/');
       ExampleContent = require('../../examples/' + name);
       ExampleContent = ExampleContent.default;
     } catch (err) {
