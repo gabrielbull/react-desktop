1225e11df31c7f3c9bea7012dbe36ade35b56aa7
Gabriel Bull <me@gabrielbull.com>
2016-08-17 09:23:25 -0400

    Fixed some styling issues with the list view

--- src/listView/macOs/style/10.11.js
+++ src/listView/macOs/style/10.11.js
@@ -2,12 +2,17 @@ export default {
   container: {
     position: 'relative',
     display: 'flex',
+    flex: 1,
+    height: '100%',
     flexDirection: 'column'
   },
 
   scrollable: {
     overflow: 'scroll',
-    width: '100%'
+    width: '100%',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column'
   },
 
   list: {
