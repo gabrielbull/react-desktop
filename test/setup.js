6559140de60080f1fc0d09478ed82abe0e7890e3
Gabriel Bull <me@gabrielbull.com>
2017-10-17 22:28:12 -0400

    Fixed unit tests

--- test/setup.js
+++ test/setup.js
@@ -2,6 +2,7 @@ import { JSDOM } from 'jsdom';
 
 const dom = new JSDOM('<!doctype html><html><body></body></html>');
 global.window = dom.window;
+global.HTMLElement = dom.window.HTMLElement;
 global.document = dom.window.document;
 global.navigator = {
   ...global.window.navigator,
