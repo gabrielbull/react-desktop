57e7e05d80ac317a249d3ed9be93e7730fe59cce
Jehad Abusir <jabusir@Jehads-MBP.lan>
2023-06-30 22:38:12 -0700

    Debug: fix window is undefined bug on next.js framework

--- src/os.js
+++ src/os.js
@@ -1,28 +1,36 @@
-export const MACOS = 'macOs';
-export const WINDOWS = 'windows';
+"use strict";
 
-export default function os() {
-  // explicitly set these to avoid issues
-  const w = window || null;
-  const n = navigator || null;
-  const p = process || (w && w.process) || null;
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+exports.default = os;
+var MACOS = (exports.MACOS = "macOs");
+var WINDOWS = (exports.WINDOWS = "windows");
+
+function os() {
+  // Check if we are running in a browser
+  var isBrowser =
+    typeof window !== "undefined" && typeof navigator !== "undefined";
+
+  // get process from window if it exists there
+  var p = isBrowser ? window.process : process;
 
   // via node
   if (p && p.platform) {
-    if (p.platform === 'darwin') {
+    if (p.platform === "darwin") {
       return MACOS;
     }
-    if (p.platform.includes('win')) {
+    if (p.platform.includes("win")) {
       return WINDOWS;
     }
   }
 
   // via user agent
-  if (n && n.userAgent) {
-    if (n.userAgent.includes('Macintosh')) {
+  if (isBrowser && navigator.userAgent) {
+    if (navigator.userAgent.includes("Macintosh")) {
       return MACOS;
     }
-    if (n.userAgent.includes('Windows')) {
+    if (navigator.userAgent.includes("Windows")) {
       return WINDOWS;
     }
   }
